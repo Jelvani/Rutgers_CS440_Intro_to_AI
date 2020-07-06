351adcb2e97500818e66a691151e04b65d2e6b27
Jelvani <Jelvani@rutgers.edu>
2020-07-06 14:43:16 -0400

    Fixed A* search bug and added backwardsA*. Also added way to save results of tests

--- pygame_test.py
+++ pygame_test.py
@@ -15,8 +15,9 @@ class Node():
         return otherNode.position == self.position
     def __lt__(self, otherNode): # use < operator to call this for nodes to test if position is less than
         if self.f == otherNode.f: #tie breaker for same f value
-            return self.g>otherNode.g
-        return self.f<otherNode.f
+            return self.g>=otherNode.g
+        else:
+            return self.f<otherNode.f
     @property
     def f(self):#automatically adds g + h when the value of f is needed, no need to manual set f=g+h
         return self.g + self.h
@@ -77,7 +78,7 @@ def createBtrackingMaze(width=101,height=101,save=False,imgfname=None,txtfname=N
         except:
             print('Error saving file with given filename')
 
-def createRandMaze(width=101,height=101,prob=0.0,save=False,imgfname=None,txtfname=None): #prob is probability of a wall appearing
+def createRandMaze(width=101,height=101,prob=0.2,save=False,imgfname=None,txtfname=None): #prob is probability of a wall appearing
     shape=(width,height)
     print('createRandMaze: maze size of ' + str(shape))
     maze = np.random.choice([0,1],size=shape,p=[1-prob,prob])
@@ -118,49 +119,55 @@ def getHeuristics(maze,goal):#returns numpy matrix of manhatten distances of eac
     for y in range(Y):
         for x in range(X):
             heaurstics[x,y] = (abs(gX-x)+abs(gY-y))
-    return heaurstics
-
-def AstarCompute(map,start,goal,heuristics, _debug=True):
+    return heaurstics  
+def AstarCompute(maze,start,goal,heuristics, _debug=True):
     startTime = time.time()
-    X,Y = map.shape
+    X,Y = maze.shape
     openList = []
-    expanded = np.zeros(map.shape, dtype=bool) #expanded matrix
+    expanded = np.zeros(maze.shape, dtype=bool) #expanded matrix
+    visited = np.zeros(maze.shape, dtype=bool) 
     startNode = Node(position=start,g = 0,h = heuristics[start])
-    goalNode = Node(position=goal,g = 0,h = 0)
+    goalNode = Node(position=goal,g = 9999,h = 0)
     heapq.heappush(openList,startNode)
+    heapq.heappush(openList,goalNode)
     visitedNodes = 0
-    expandedNodes = 0
     while(openList):
         '''get node in openlist with lowest f value'''
         current=heapq.heappop(openList)
+        if expanded[current.position]==1:#if already explored, pop another node
+            continue
         expanded[current.position]=1
+        
         A,B = current.position
         '''right after appending to closed list, check if appended node is the goal and return if it is'''
         if current == goalNode:
             break
-        expandedNodes = expandedNodes + 1
-        for x in [(1,0),(-1,0),(0,1),(0,-1)]:#TODO: add map and expanded into one common boolean list
-            if A+x[0] in range(X) and B+x[1] in range(Y) and map[A+x[0],B+x[1]]==0 and expanded[A+x[0],B+x[1]]==0:#if in range and not a wall, add to temporary list
+        for x in [(1,0),(0,1),(-1,0),(0,-1)]:#TODO: add map and expanded into one common boolean list
+            if A+x[0] in range(X) and B+x[1] in range(Y) and maze[A+x[0],B+x[1]]==0:#if in range and not a wall, add to temporary list
                 newNode = Node(parent=current, position=(A+x[0],B+x[1]), g=current.g+1, h=heuristics[(A+x[0],B+x[1])]) 
+                visited[A+x[0],B+x[1]]=1
                 visitedNodes = visitedNodes +1
                 heapq.heappush(openList,newNode)            
     endTime = time.time()
+    if not openList:
+        print('AstarCompute: No solution')
+        return None
     if _debug:
         print('AstarCompute: time to A* search maze was %s seconds' %(endTime-startTime))
         print('AstarCompute: %s nodes visited' %visitedNodes)
-        print('AstarCompute: %s nodes expanded' %expandedNodes)
+        print('AstarCompute: %s nodes expanded' %np.count_nonzero(expanded))
     return current
 
-def repeatedForwardAstar(map, start, goal):
+def repeatedForwardAstar(maze, start, goal):
     startTime = time.time()
     current = start
-    X,Y = map.shape
-    heur = getHeuristics(map,goal)
-    seenMap = np.zeros(map.shape, dtype=bool) #map with only seen walls
-    followedMap = np.zeros(map.shape, dtype=bool) #used to keep track of travelled path
+    X,Y = maze.shape
+    heur = getHeuristics(maze,goal)
+    seenMap = np.zeros(maze.shape, dtype=bool) #map with only seen walls
+    followedMap = np.zeros(maze.shape, dtype=bool) #used to keep track of travelled path
     path = [] #current path being executed by latest A* call
-    nodes = AstarCompute(map=seenMap,start=current,goal=goal,heuristics=heur,_debug=False) #initial path on empty (no wall) map
-    while nodes is not None: #get array of positions to travel from A* linked liost
+    nodes = AstarCompute(maze=seenMap,start=current,goal=goal,heuristics=heur,_debug=False) #initial path on empty (no wall) map
+    while nodes is not None: #get array of positions to travel from A* linked list
         path.append(nodes.position)
         nodes=nodes.parent
 
@@ -168,44 +175,97 @@ def repeatedForwardAstar(map, start, goal):
         A,B = current
         ''' update seen map with only adjacent walls that are visible '''
         for x in [(1,0),(-1,0),(0,1),(0,-1)]:
-            if A+x[0] in range(X) and B+x[1] in range(Y) and map[A+x[0],B+x[1]]==1:
+            if A+x[0] in range(X) and B+x[1] in range(Y) and maze[A+x[0],B+x[1]]==1:
                 seenMap[A+x[0],B+x[1]] = 1
+        
         ''' if new walls seen on current path, then compute A* again'''
         if seenMap[path[-1]] == 1:
             path.clear()
-            nodes = AstarCompute(map=seenMap,start=current,goal=goal,heuristics=heur,_debug=False)
-            while nodes.parent is not None:#get list of path to follow from linked list
+            nodes = AstarCompute(maze=seenMap,start=current,goal=goal,heuristics=heur,_debug=False)
+            if nodes is None:
+                return None
+            while nodes is not None:#get list of path to follow from linked list
                 path.append(nodes.position)
                 nodes=nodes.parent
-        
         current = path.pop()
         followedMap[current] = 1
     endTime = time.time()
     print('repeatedForwardAstar: time to A* search maze was %s seconds' %(endTime-startTime))
     print('repeatedForwardAstar: %s nodes travelled' %np.count_nonzero(followedMap))
     return followedMap
-        
-maze=loadmaze(num=1)
+
+def repeatedBackwardAstar(maze, start, goal):
+    startTime = time.time()
+    current = start
+    X,Y = maze.shape
+    heur = getHeuristics(maze,current)
+    seenMap = np.zeros(maze.shape, dtype=bool) #map with only seen walls
+    followedMap = np.zeros(maze.shape, dtype=bool) #used to keep track of travelled path
+    path = [] #current path being executed by latest A* call
+    nodes = AstarCompute(maze=seenMap,start=goal,goal=current,heuristics=heur,_debug=False) #initial path on empty (no wall) map
+    while nodes is not None: #get array of positions to travel from A* linked list
+        path.append(nodes.position)
+        nodes=nodes.parent
+    
+    while current != goal:
+        A,B = current
+        ''' update seen map with only adjacent walls that are visible '''
+        for x in [(1,0),(-1,0),(0,1),(0,-1)]:
+            if A+x[0] in range(X) and B+x[1] in range(Y) and maze[A+x[0],B+x[1]]==1:
+                seenMap[A+x[0],B+x[1]] = 1
+        ''' if new walls seen on current path, then compute A* again'''
+        if seenMap[path[0]] == 1:
+            path.clear()
+            heur = getHeuristics(maze,current)
+            nodes = AstarCompute(maze=seenMap,start=goal,goal=current,heuristics=heur,_debug=False)
+            if nodes is None:
+                return None
+            while nodes is not None:#get list of path to follow from linked list
+                path.append(nodes.position)
+                nodes=nodes.parent
+        current = path.pop(0)
+        followedMap[current] = 1
+    endTime = time.time()
+    print('repeatedBackwardAstar: time to A* search maze was %s seconds' %(endTime-startTime))
+    print('repeatedBackwardAstar: %s nodes travelled' %np.count_nonzero(followedMap))
+    return followedMap
+
+def performTests():
+    file1 = open("generated_mazes/Tests.txt","w") 
+    for x in range(24):
+        x=x+25
+        print('performTests: %s' %x)
+        file1.write("Test %s \n" %x)
+        maze=loadmaze(num=x)
+        A,B = maze.shape
+        goal=(100,100)
+        start=(0,0)
+        heur = getHeuristics(maze,goal)
+        seenmap = np.zeros(maze.shape, dtype=bool)
+        startTime = time.time()
+        seenmap = repeatedForwardAstar(maze,start,goal)
+        if seenmap is None:
+            print('performTests: No solution')
+            file1.write("No solution \n")
+        endTime = time.time()
+        print('performTests: Took %s seconds' %(endTime-startTime))
+        file1.write("%s \n" %(endTime-startTime))
+    file1.close() 
+
+'''
+maze=loadmaze(num=36)  
 A,B = maze.shape
 goal=(100,100)
 start=(0,0)
 heur = getHeuristics(maze,goal)
 seenmap = np.zeros(maze.shape, dtype=bool)
+#startTime = time.time()
 seenmap = repeatedForwardAstar(maze,start,goal)
-'''
-nodes = AstarCompute(maze,start,goal,heur)
-count = 0
-while nodes.parent is not None:
-    seenmap[nodes.position] = 1
-    print(nodes.position)
-    nodes=nodes.parent
-    count = count + 1
-
-print(count)
-'''
-seenmap[start]=200
-seenmap[goal]=200
-plt.imshow(maze,cmap=plt.cm.binary)
+#seenmap = repeatedBackwardAstar(maze,start,goal)
+plt.figure()
+plt.imshow(maze, cmap=plt.cm.binary, interpolation='nearest')
 plt.imshow(seenmap,alpha=0.5)
 plt.xticks([]), plt.yticks([])
 plt.show()
+'''
+performTests()
